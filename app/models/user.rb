e66f8385cdbaef977fe0bc89628f59e241c074ce
trntkh <hongtran131199@gmail.com>
2023-07-08 17:13:27 +0700

    Create enum status for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :comments
   mount_uploader :avatar, AvatarUploader
   enum user_type: {employee: 0, manager: 1, admin: 2}
+  enum status: {active: 0, inactive: 1, block: 2}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
